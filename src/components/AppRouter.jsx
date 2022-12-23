d5ebc90943590767148dc32c897dfdec2a805233
Slepov Vladimir <slepovva@gmail.com>
2022-12-23 15:13:19 +0700

    add post page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,12 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import About from "../pages/About";
 import Post from "../pages/Post";
 import Error from "../pages/Error";
+import PostPage from "./PostPage";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About/>}/>
             <Route path="/posts" element={<Post/>}/>
+            <Route path="/posts/:id" element={<PostPage/>}/>
             <Route path="/error" element={<Error/>}/>
             <Route path="*" element={<Error/>}/>
         </Routes>
